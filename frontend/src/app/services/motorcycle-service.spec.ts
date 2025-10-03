a851809a12173a1d682bd753cd21d988916c7616
YeremiP <jeremypaul476@gmail.com>
2025-10-03 20:44:42 +0100

    Update: convert text from Spanish to English

--- frontend/src/app/services/motorcycle-service.spec.ts
+++ frontend/src/app/services/motorcycle-service.spec.ts
@@ -1,20 +1,3 @@
-// import { TestBed } from '@angular/core/testing';
-
-// import { MotorcycleService } from './motorcycle-service';
-
-// describe('MotorcycleService', () => {
-//   let service: MotorcycleService;
-
-//   beforeEach(() => {
-//     TestBed.configureTestingModule({});
-//     service = TestBed.inject(MotorcycleService);
-//   });
-
-//   it('should be created', () => {
-//     expect(service).toBeTruthy();
-//   });
-// });
-
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
